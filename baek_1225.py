63aa925ed4117b882418ea752b0b9cb622aa7191
Baeyonghyeon <qodydgus97@naver.com>
2021-10-15 22:41:29 +0900

    이상한 곱셈

--- baek_1225.py
+++ baek_1225.py
@@ -1,6 +1,15 @@
 def main():
-    
+    token = input().split(" ")
+    A = token[0]
+    B = token[1]
+    Atemp = 0
+    Btemp = 0
 
+    for i in range(len(A)):
+        Atemp += int(A[i])
+    for j in range(len(B)):
+        Btemp += int(B[j])
 
+    print(Atemp * Btemp)
 if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def main():
-    print(chr(97))
+    x = int(input("정수 1 :"))
+    y = int(input("정수 2 :"))
+    print("큰 수 : ",max(x,y))
+    print("작은 수 :",min(x,y))
+    print("차의 절댓값 : ", abs(x-y))
+
+
 
 if __name__ == '__main__':
     main()
